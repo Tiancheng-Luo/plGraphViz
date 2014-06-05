acf4ec31ef12d0de84f0496e621a90dcec5ae55c
Wouter Beek <me@wouterbeek.com>
2014-06-05 08:38:17 +0200

    * [GV_ATTRS,GV_COLOR,NEW] Added persistency, so GraphViz settings are (re)download only when the locally stored data dump is not fresh anymore.

--- gv_attrs.pl
+++ gv_attrs.pl
@@ -14,15 +14,20 @@
 */
 
 :- use_module(library(apply)).
+:- use_module(library(persistency)).
 :- use_module(library(xpath)).
 
 :- use_module(dcg(dcg_content)).
 :- use_module(dcg(dcg_generic)).
+:- use_module(generics(db_ext)).
+:- use_module(os(file_ext)).
 
 :- use_module(plHtml(html)).
 :- use_module(plHtml(html_table)).
 
 :- use_module(plGraphViz(gv_attr_type)). % DCGs implementing attribute types.
+
+:- db_add_novel(user:prolog_file_type(log, logging)).
 
 %! gv_attr(
 %!   ?Name:atom,
@@ -33,9 +38,18 @@
 %!   ?Notes:atom
 %! ) is nondet.
 
-:- dynamic(gv_attr/6).
+:- persistent(
+  gv_attr(
+    name:atom,
+    used_by:list(oneof([cluster,edge,graph,node,subgraph])),
+    types:list(atom),
+    default,
+    minimum,
+    notes:atom
+  )
+).
 
-:- initialization(gv_attrs_download).
+:- initialization(gv_attrs_init).
 
 
 
@@ -64,7 +78,20 @@ check_minimum(V, Min1):-
 
 
 
-% Download attributes from graphviz.org
+% INITIALIZATION
+
+%! assert_gv_attr_row(+Row:list(atom)) is det.
+
+assert_gv_attr_row([Name,UsedBy1,Types1,Default1,Minimum,Notes]):-
+  dcg_phrase(translate_usedby(UsedBy2), UsedBy1),
+  once(dcg_phrase(translate_type(Types2), Types1)),
+  sort(UsedBy2, UsedBy3),
+  translate_default(Default1, Default2),
+  assert_gv_attr(Name, UsedBy3, Types2, Default2, Minimum, Notes).
+
+
+%! gv_attrs_downloads is det.
+% Downloads the table describing GraphViz attributes from `graphviz.org`.
 
 gv_attrs_download:-
   gv_attrs_url(Url),
@@ -76,16 +103,56 @@ gv_attrs_download:-
   %%%%xpath(Dom, /html/body/table, TableDom),
 
   html_to_table(TableDom, _, Rows),
-  maplist(assert_gv_attr, Rows).
+  maplist(assert_gv_attr_row, Rows).
+
+
+%! gv_attrs_file(-File:atom) is det.
+
+gv_attrs_file(File):-
+  absolute_file_name(
+    data(gv_attrs),
+    File,
+    [access(write),file_type(logging)]
+  ).
+
+
+%! gv_attrs_init is det.
+
+gv_attrs_init:-
+  gv_attrs_file(File),
+  safe_db_attach(File),
+  file_age(File, Age),
+  gv_attrs_update(Age).
+
+
+%! gv_attrs_update(+Age:float) is det.
+
+% The persistent store is still fresh.
+gv_attrs_update(Age):-
+  once(gv_attr(_, _, _, _, _, _)),
+  Age < 3600, !.
+% The persistent store has become stale, so refresh it.
+gv_attrs_update(_):-
+  retractall_gv_attr(_, _, _, _, _, _),
+  gv_attrs_download.
+
+
+%! gv_attrs_url(-Url:url) is det.
 
 gv_attrs_url('http://www.graphviz.org/doc/info/attrs.html').
 
-assert_gv_attr([Name,UsedBy1,Types1,Default1,Minimum,Notes]):-
-  dcg_phrase(translate_usedby(UsedBy2), UsedBy1),
-  once(dcg_phrase(translate_type(Types2), Types1)),
-  sort(UsedBy2, UsedBy3),
-  translate_default(Default1, Default2),
-  assert(gv_attr(Name, UsedBy3, Types2, Default2, Minimum, Notes)).
+
+%! safe_db_attach(+File:atom) is det.
+
+safe_db_attach(File):-
+  exists_file(File), !,
+  db_attach(File, []).
+safe_db_attach(File):-
+  touch_file(File),
+  safe_db_attach(File).
+
+
+%! translate_default(+Default1:atom, -Default2:atom) is det.
 
 % The empty string is represented by the empty atom.
 translate_default('""', ''):- !.
@@ -93,12 +160,20 @@ translate_default('""', ''):- !.
 translate_default('<none>', _):- !.
 translate_default(Default, Default).
 
+
+%! translate_type(-Types:list(atom))// is det.
+
 translate_type([H|T]) -->
   {gv_attr_type(H)},
   atom(H),
   whites,
   translate_type(T).
-translate_type([]) --> [].
+translate_type([]) --> !, [].
+
+
+%! translated_usedby(
+%!   -UsedBy:list(oneof([cluster,edge,graph,node,subgraph]))
+%! )// is det.
 
 translate_usedby([cluster|T]) --> `C`, !, translate_usedby(T).
 translate_usedby([edge|T]) --> `E`, !, translate_usedby(T).
--- gv_color.pl
+++ gv_color.pl
@@ -18,20 +18,25 @@
 
 :- use_module(library(apply)).
 :- use_module(library(lists)).
+:- use_module(library(persistency)).
 :- use_module(library(xpath)).
 
 :- use_module(dcg(dcg_abnf)).
 :- use_module(dcg(dcg_cardinal)).
+:- use_module(generics(db_ext)).
+:- use_module(os(file_ext)).
 :- use_module(sparql(sparql_char)).
 
 :- use_module(plHtml(html)).
 :- use_module(plHtml(html_table)).
 
+:- db_add_novel(user:prolog_file_type(log, logging)).
+
 %! gv_color(?Colorscheme:oneof([svg,x11]), ?Color:atom) is nondet.
 
-:- dynamic(gv_color/2).
+:- persistent(gv_color(colorscheme:oneof([svg,x11]),color:atom)).
 
-:- initialization(gv_color_download).
+:- initialization(gv_color_init).
 
 
 
@@ -75,9 +80,9 @@ wc_weight(Float) -->
 
 
 
-% Initialization.
+% INITIALIZATION
 
-gv_color_url('http://www.graphviz.org/doc/info/colors.html').
+%! gv_color_download is det.
 
 gv_color_download:-
   gv_color_url(Url),
@@ -91,6 +96,52 @@ assert_color_table(Colorscheme, TableDom):-
   append(Rows, Cells),
   forall(
     member(Cell, Cells),
-    assert(gv_color(Colorscheme, Cell))
+    assert_gv_color(Colorscheme, Cell)
   ).
+
+
+%! gv_color_file(-File:atom) is det.
+
+gv_color_file(File):-
+  absolute_file_name(
+    data(gv_color),
+    File,
+    [access(write),file_type(logging)]
+  ).
+
+
+%! gv_color_init is det.
+
+gv_color_init:-
+  gv_color_file(File),
+  safe_db_attach(File),
+  file_age(File, Age),
+  gv_color_update(Age).
+
+
+%! gv_color_update(+Age:float) is det.
+
+% The persistent store is still fresh.
+gv_color_update(Age):-
+  once(gv_color(_, _)),
+  Age < 3600, !.
+% The persistent store has become stale, so refresh it.
+gv_color_update(_):-
+  retractall_gv_color(_, _),
+  gv_color_download.
+
+
+%! gv_color_url(-Url:url) is det.
+
+gv_color_url('http://www.graphviz.org/doc/info/colors.html').
+
+
+%! safe_db_attach(+File:atom) is det.
+
+safe_db_attach(File):-
+  exists_file(File), !,
+  db_attach(File, []).
+safe_db_attach(File):-
+  touch_file(File),
+  safe_db_attach(File).
 
