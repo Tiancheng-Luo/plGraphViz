ebb86cdfd67c1bc273905eb28be11d152520110e
Wouter Beek <me@wouterbeek.com>
2014-10-31 21:02:15 +0100

    DEPLOYMENT: Updated to modern startup architecture.

---
+++ debug.pl
@@ -0,0 +1,7 @@
+% Loads debug tools for the plGraphViz library.
+
+:- [load].
+
+:- use_module(debug_project).
+:- debug_all_files.
+
--- load.pl
+++ load.pl
@@ -1,12 +1,13 @@
-% Load file for plGraphViz.
+% Load file for the plGraphViz library.
 
-:- dynamic(user:prolog/3).
-:- multifile(user:prolog/3).
-   user:project(plGraphViz, 'GraphViz support for SWI-Prolog.', plGraphViz).
+:- dynamic(user:project/3).
+:- multifile(user:project/3).
+   user:project(plGraphViz, 'GraphViz support for SWI-Prolog.').
 
 :- use_module(load_project).
-:- load_project(plGraphViz, [
+:- load_project([
     plc-'Prolog-Library-Collection',
+    plDcg,
     plHtml
 ]).
 
