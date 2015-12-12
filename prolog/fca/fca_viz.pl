f16acca2418fbb9e0d879e9ef5e90bbf48fdb41e
Wouter Beek <me@wouterbeek.com>
2015-12-13 00:31:18 +0100

    CLEAN: Simplified FCA concept label writing.

--- prolog/fca/fca_viz.pl
+++ prolog/fca/fca_viz.pl
@@ -28,23 +28,19 @@
 :- use_module(library(option)).
 :- use_module(library(ordsets)).
 
-:- meta_predicate(concept_label(+,1,1,+,-)).
-:- meta_predicate(concept_label(+,1,1,+,?,?)).
 :- meta_predicate(fca_export_graph(+,?,:)).
 :- meta_predicate(fca_viz(+,?,:)).
 
 :- predicate_options(fca_export_graph/3, 3, [
-     attribute_label(+callable),
-     concept_label(+oneof([attributes,both,objects])),
-     object_label(+callable)
+     concept_label(+callable)
    ]).
 :- predicate_options(fca_viz/3, 3, [
      pass_to(fca_export_graph/3, 3),
      pass_to(gv_export/3, 3)
    ]).
 
-is_meta(attribute_label).
-is_meta(object_label).
+is_meta(concept_label).
+
 
 
 
@@ -62,26 +58,16 @@ fca_export_graph(Context, ExportG):-
 %!   :Options:list(compound)
 %! ) is det.
 % The following optios are supported:
-%   * attribute_label(+callable)
-%     DCG writing the labels for individual attributes.
-%     Default is pl_term//1.
-%   * concept_label(+oneof([attributes,both,objects]))
-%     Determines which components of the concepts are displayed
-%     in the export graph.
-%     Default is `both`.
-%   * object_label(+callable)
-%     DCG writing the labels for individual objects.
-%     Default is pl_term//1.
+%   * concept_label(+callable)
+%     DCG writing the labels for individual concepts.
 
 fca_export_graph(Context, ExportG, Opts1):-
   fca_hasse(Context, Hasse),
   meta_options(is_meta, Opts1, Opts2),
-  option(attribute_label(ALbl_1), Opts2, pl_term),
-  option(concept_label(Mode), Opts2, both),
-  option(object_label(OLbl_1), Opts2, pl_term),
+  option(concept_label(Label_1), Opts2, concept_label),
   merge_options(
     [
-      vertex_label(fca_viz:concept_label(Mode, OLbl_1, ALbl_1)),
+      vertex_label(Label_1),
       vertex_rank(fca:concept_cardinality)
     ],
     Opts2,
@@ -107,31 +93,12 @@ fca_viz(Context, File, Opts1):-
 
 
 
-%! concept_label(
-%!   +Mode:oneof([attributes,both,objects]),
-%!   :ObjectLabel_1,
-%!   :AttributeLabel_1,
-%!   +Concept:compound,
-%!   -Label:atom
-%! ) is det.
+%! concept_label(+Concept:compound, -Label:atom) is det.
 
-concept_label(Mode, OLbl_1, ALbl_1, Concept, Lbl):-
-  string_phrase(concept_label(Mode, OLbl_1, ALbl_1, Concept), Lbl).
+concept_label(Concept, Lbl):-
+  string_phrase(concept_label(Concept), Lbl).
 
 
-%! concept_label(
-%!   +Mode:oneof([attributes,both,objects]),
-%!   :ObjectLabel_1,
-%!   :AttributeLabel_1,
-%!   +Concept:compound
-%! )// is det.
+%! concept_label(+Concept:compound)// is det.
 
-concept_label(attributes, _, ALbl_1, concept(_,As)) -->
-  set(ALbl_1, As).
-concept_label(both, OLbl_1, ALbl_1, Concept) -->
-  pair(
-    concept_label(objects, OLbl_1, ALbl_1, Concept),
-    concept_label(attributes, OLbl_1, ALbl_1, Concept)
-  ).
-concept_label(objects, OLbl_1, _, concept(Os,_)) -->
-  set(OLbl_1, Os).
+concept_label(concept(Os,As)) --> set(Os), " / ", set(As).
