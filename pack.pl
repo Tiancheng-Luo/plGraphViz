41914a2d39ceca347ebcc4785cf5615c9c7f8376
Wouter Beek <me@wouterbeek.com>
2016-05-03 20:54:24 +0200

    pack

--- pack.pl
+++ pack.pl
@@ -4,6 +4,6 @@ home('https://github.com/wouterbeek/plGraphViz').
 maintainer('Wouter Beek', 'me@wouterbeek.com').
 name(plGraphViz).
 packager('Wouter Beek', 'me@wouterbeek.com').
-requires('Prolog_Library_Collection').
+requires('Prolog-Library-Collection').
 title(plGraphViz).
-version('0.0.17').
+version('0.0.1').
